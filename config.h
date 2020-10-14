1ec02c44dc57f9f1a1886a9000b84670bbea9c22
ProfessorPenultimate <professorpenultimate@gmail.com>
2020-10-13 20:32:05 -0700

    Don't test keyboards with nano open ;)

--- config.h
+++ config.h
@@ -49,7 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
- 1*1111111/
+ */
 //#define SOFT_SERIAL_PIN E6  // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN A13
