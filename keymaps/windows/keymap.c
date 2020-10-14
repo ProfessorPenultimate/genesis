e169776dbd607b52351fa25f97673ff8053007eb
ProfessorPenultimate <professorpenultimate@gmail.com>
2020-10-14 06:36:40 -0700

    Updated licensing

--- keymaps/windows/keymap.c
+++ keymaps/windows/keymap.c
@@ -1,5 +1,4 @@
-
-/* Copyright 2020 Matt3o
+/* Copyright 2020 ProfessorPenulimate
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
